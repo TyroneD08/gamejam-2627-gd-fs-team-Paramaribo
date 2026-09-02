b9683d7c7a6efa046caced5babe28378550cea6e
Milad Sahar <31002@ma-web.nl>
2026-09-02 14:13:11 +0200

    Changed Unity Version

--- gj-2627-slotmachine/Assets/Scripts/PlayerController.cs
+++ gj-2627-slotmachine/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    InputAction P1Roll;
+    InputAction P2Roll;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        P1Roll = InputSystem.actions.FindAction("P1Roll");
+        P2Roll = InputSystem.actions.FindAction("P2Roll");
     }
 
     // Update is called once per frame
     void Update()
     {
-         
+        if (P1Roll.IsPressed())
+        {
+            Debug.Log("Player 1 rolled.");
+        }
+        
+        if (P2Roll.IsPressed())
+        {
+            Debug.Log("Player 2 rolled.");
+        }
     }
 }
